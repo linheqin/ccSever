7c9de846a4450fcfef77a00090f2b57745571cf7
linheqin <592732096@qq.com>
2018-03-01 15:49:04 +0800

    增加一些接口

--- js/common.js
+++ js/common.js
@@ -1,8 +1,47 @@
+// 前缀参数
 var prefixParamUrl =  {
     urlPrefix : "http://www.argen3630.com:88/"
 };
+// API接口路径
 var urlParam = {
-    register: prefixParamUrl.urlPrefix +"company/register",                         // 注册接口
-    login : prefixParamUrl.urlPrefix + "admin.php/Login/index",                     // 登录
-    getPeopleInfo: prefixParamUrl.urlPrefix + "admin.php/Member/get"                // 个人信息
+    register: prefixParamUrl.urlPrefix + "admin.php/Company/register",              // 注册接口
+    login: prefixParamUrl.urlPrefix + "admin.php/Login/index",                      // 登录
+    getPeopleInfo: prefixParamUrl.urlPrefix + "admin.php/Member/get",               // 个人信息
+    modifyPwd: prefixParamUrl.urlPrefix + "admin.php/Member/resetPassword",         // 修改个密码
+    orderList: prefixParamUrl.urlPrefix + "admin.php/Order/list1",                  // 获取订单列表,
+    modifyPeoleInfo: prefixParamUrl.urlPrefix + "admin.php/Member/modify",          // 修改个人信息
+    getProductList: prefixParamUrl.urlPrefix + "admin.php/Product/list1",           // 修改个人信息
+    getProductDetaile: prefixParamUrl.urlPrefix + "admin.php/Product/get",          // 获取套餐详情,
+    getWebsiteList: prefixParamUrl.urlPrefix + "admin.php/Website/list1",           // 获取站点列表,
+    addWebsite: prefixParamUrl.urlPrefix + "admin.php/Website/add",                 // 增加站点,
+    webSiteDelelte: prefixParamUrl.urlPrefix + "admin.php/Website/delelte",         // 删除站点,
+    modifyWebSite: prefixParamUrl.urlPrefix + "admin.php/Website/modify",           // 修改站点,
+}
+// 获取地址参数
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = parent.window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+// 获取当前iframe的地址参数
+function getIframeQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+// 弹创提示
+function msgMask(val,type){
+    if(type == 1) {
+        parent.layer.msg(val,{
+            shade: 0.2,
+            time: 1000,
+        });
+    } else {
+        layer.msg(val,{
+            shade: 0.2,
+            time: 1000,
+        });
+    }
 }
---
+++ js/nav_setting_center.js
@@ -0,0 +1,119 @@
+
+$(function(){
+    //PC窗口设置(咨询图标）——自定义颜色块个数 
+    var askLiColorBars = $(".askdefineColorbars").children('li');
+    askLiColorBars.click(function(){
+        var bgColor = $(this).css("backgroundColor");
+        $(".askdefinecolorspan").css("backgroundColor",bgColor);
+        $(".colorRBar").css("backgroundColor",bgColor);
+        $.ajax({
+            url: '/path/to/file',
+            type: 'POST',
+            dataType: 'json',
+            data: {param1: 'bgColor'},
+        })
+    })
+
+    //PC窗口设置(访客对话窗口）——自定义颜色块个数 
+    var visiterLiColorBars = $(".visiterdefineColorbars").children('li');
+    visiterLiColorBars.click(function(){
+        var bgColor = $(this).css("backgroundColor");
+        $("#theme_color_show").css("backgroundColor",bgColor);
+        $("#chatwin_view_bg").css("backgroundColor",bgColor);
+        $.ajax({
+            url: '/path/to/file',
+            type: 'POST',
+            dataType: 'json',
+            data: {param1: 'bgColor'},
+        })
+    })
+    //PC窗口设置(直接对话窗口）——自定义颜色块个数 
+    var directLiColorBars = $(".directdefineColorbars").children('li');
+    directLiColorBars.click(function(){
+        var bgColor = $(this).css("backgroundColor");
+        $(".themecolorshowspan").css("backgroundColor",bgColor);
+        $("#mini_chatwin_view_bg").css("backgroundColor",bgColor);
+        $("#mini_chatwin_view_bg_lite").css("backgroundColor",bgColor);
+        $.ajax({
+            url: '/path/to/file',
+            type: 'POST',
+            dataType: 'json',
+            data: {param1: 'bgColor'},
+        })
+    })
+    //手机窗口设置(邀请窗口）——自定义颜色块个数 
+    var inviteLiColorBars = $(".invitedefineColorbars").children('li');
+    inviteLiColorBars.click(function(){
+        var bgColor = $(this).css("backgroundColor");
+        $(".invitedefinecolorspan").css("backgroundColor",bgColor);
+        $(".qy_kf_welcomeBox_words").css("backgroundColor",bgColor);
+        $.ajax({
+            url: '/path/to/file',
+            type: 'POST',
+            dataType: 'json',
+            data: {param1: 'bgColor'},
+        })
+    })
+
+
+
+    //弹窗部分
+    //咨询图标
+    $("#adv_addbotlabel,#adv_item_add_div").click(function(){
+        layer.open({
+          type: 1,
+          shade:0,
+          skin: 'addElePop', 
+          area: '430px',
+          title:'添加元素',
+          content: $('#addElePopOuter'),
+
+        });
+    })
+    // 常用文件
+    $("#add_folder").click(function(){
+        layer.open({
+          type: 1,
+          shade:0,
+          skin: 'createFilesPop', 
+          area: ['280px','150px'],
+          title:'新建文件夹',
+          content: $('#createFilesPopOuter'),
+
+        });
+    })
+
+    // 微信互通，切换按钮
+    var indexBoolen = 1;
+    $(".switchery").click(function(){
+        var jSitch = $(".js-switch").attr('checked');
+        console.log(jSitch);
+        if(indexBoolen == 1){
+            $(".switchery small").css({
+                'left': '30px'
+            })
+            $(".switchery").css({
+                'border-color': '#00BC0C',
+                'box-shadow': '#00BC0C 0px 0px 0px 16px inset',
+                'transition': 'border 0.4s, box-shadow 0.4s, background-color 1.2s',
+                'background-color': '#00BC0C'
+            })
+            $(".js-switch").attr('checked',true);
+            indexBoolen = 0
+        }else if(indexBoolen == 0){
+            $(".switchery small").css({
+                'left': '0px'
+            })
+            $(".switchery").css({
+                'border-color': '#dfdfdf',
+                'box-shadow': '#fff 0px 0px 0px 16px inset',
+                'transition': 'border 0.4s, box-shadow 0.4s, background-color 1.2s',
+                'background-color': '#fff'
+            })
+            $(".js-switch").attr('checked',false);
+            indexBoolen = 1;
+        }
+    })
+    
+})
+
